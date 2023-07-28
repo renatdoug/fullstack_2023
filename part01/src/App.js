421bcefa5ebf0ea5413f7716f70827a1e1e74317
Renato Douglas <renatdoug@hotmail.com>
2023-07-28 13:13:52 -0300

    second_step

--- part01/src/App.js
+++ part01/src/App.js
@@ -6,16 +6,18 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.parts[0].name} {props.parts[0].exercises}
-      </p>
-      <p>
-        {props.parts[1].name} {props.parts[1].exercises}
-      </p>
-      <p>
-        {props.parts[2].name} {props.parts[2].exercises}
-      </p>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
   );
 };
 
